82dd859ae5b77bac36bf9a97f754f21d504e67a8
Muhammad Waseem <waseem@xertk.com>
2024-01-30 17:22:10 +0500

    cutom api and error response

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("Mongo DB connection Failed !!!", err));
